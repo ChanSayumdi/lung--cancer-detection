33509aa4cab72e5ca43743cdd71820cadc7342e5
withanageyasiru <withanageyasiru@gmail.com>
2020-10-26 11:18:44 +0530

    add filters to trail

--- Classifier/dataloder/DataLoader.py
+++ Classifier/dataloder/DataLoader.py
@@ -3,7 +3,7 @@ import re
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-import cv2
+import cv2 as cv
 import pydicom as dicom
 from skimage import exposure
 
@@ -15,14 +15,31 @@ class DataLoader:
         fileDir = os.path.dirname(os.path.realpath('__file__'))
         data_path= os.path.join(fileDir, path)
         filenames=os.listdir(data_path)
-        ds=dicom.dcmread(os.path.join( data_path , filenames[1]))
-        dcm_sample=ds.pixel_array
-        dcm_sample=exposure.equalize_adapthist(dcm_sample)
-        cv2.imshow('sample image dicom',dcm_sample)
-        cv2.waitkey()
+        ds = []
+        fs = []
+        for i in filenames:
+            try:
+                file_path = os.path.join( data_path , i)
+                dcm_sample = dicom.dcmread(file_path).pixel_array
+                dcm_sample=exposure.equalize_adapthist(dcm_sample)
+                # Convert to float to avoid overflow or underflow losses.
+                image_2d = dcm_sample.astype(float)
+
+                # Rescaling grey scale between 0-255
+                image_2d_scaled = (np.maximum(image_2d,0) / image_2d.max()) * 255.0
+
+                # Convert to uint
+                image_2d_scaled = np.uint8(image_2d_scaled)
+                ds.append(image_2d_scaled)
+                fs.append(i)
+            except:
+                print("just ignore it")
+
         
-        
-        x_train =  None
+        # print(image_2d_scaled)
+        # self.debugDisplay(image_2d_scaled)
+
+        x_train =  ds
         y_train =  None
         return x_train,y_train 
 
@@ -35,11 +52,12 @@ class DataLoader:
 
         dataSet = train_df.sample(frac=1) # Shuffle data
         file_path = [os.path.join( data_path , i) for i in dataSet.filename.tolist()] 
-        images = [cv2.imread(i, cv2.IMREAD_GRAYSCALE) for i in file_path]
+        images = [cv.imread(i, cv.IMREAD_GRAYSCALE) for i in file_path]
         x_train =  np.array(images)
         y_train =  dataSet['class'].to_numpy()
         return x_train,y_train 
 
     def debugDisplay(self,image):
-        plt.imshow(image, cmap='gray')  # graph it
-        plt.show()
+        cv.imshow('sample image dicom',image)
+        cv.waitKey()
+
